26235464809dc6b59172c0b604dc1146d910a47e
softwareYusuf <112049106+softwarEngineerYusuf@users.noreply.github.com>
2024-03-26 23:30:48 +0300

    react router yüklendi

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
-
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-
-
   return (
-    <>
-     
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Blogs />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
