cda864672f273508a7a3471aa72ec81acc5387e2
softwareYusuf <112049106+softwarEngineerYusuf@users.noreply.github.com>
2024-03-27 01:22:46 +0300

    logo has added

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginMain from "./Pages/LoginMain";
+import Navbar from "./Component/Navbar";
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<LoginMain />} />
       </Routes>
---
+++ front-end/src/Component/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+import Logo from "../assets/sksLogo.jpeg";
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar">
+        <img src={Logo} alt="Logo" className="logo" />
+        <div className="LinkDiv">
+          <NavLink to="/" className="nav-link">
+            Clubs
+          </NavLink>
+          <NavLink to="/" className="nav-link">
+            Announcements
+          </NavLink>
+          <NavLink to="/" className="nav-link">
+            Contact Us
+          </NavLink>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
