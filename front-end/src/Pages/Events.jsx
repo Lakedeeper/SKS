d9a8755099107da929ab380fd04bd3a7eeede719
Arif Halit Arslan <arifarslan@Arif-MacBook-Air.local>
2024-03-27 23:33:34 +0300

    Events Page has been changed

--- front-end/src/Pages/Events.jsx
+++ front-end/src/Pages/Events.jsx
@@ -4,51 +4,40 @@ import './Events.css'
 
 function Events() {
   return (
-    
-<div className="card-group">
-  <div className="card">
-    <img src="..." className="card-img-top" alt="..." />
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </p>
-      <p className="card-text">
-        <small className="text-body-secondary">Last updated 3 mins ago</small>
-      </p>
-    </div>
-  </div>
-  <div className="card">
-    <img src="..." className="card-img-top" alt="..." />
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-        This card has supporting text below as a natural lead-in to additional
-        content.
-      </p>
-      <p className="card-text">
-        <small className="text-body-secondary">Last updated 3 mins ago</small>
-      </p>
-    </div>
-  </div>
-  <div className="card">
-    <img src="..." className="card-img-top" alt="..." />
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </p>
-      <p className="card-text">
-        <small className="text-body-secondary">Last updated 3 mins ago</small>
-      </p>
-    </div>
-  </div>
-</div>
 
+<div className="container">
+      <div className="row">
+        <h1 className='EventsHeader' style={{ marginTop: "75px" }}>EVENTS</h1>
+        <div className="col-md-3">
+          <div className="card cardEvent">
+            <img
+              src="https://uskudar.edu.tr/assets/uploads/ogrencikulup/222/uskubat-kulubu.png"
+              alt="Resim"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Kart Başlık 1</h5>
+              <p className="card-text">Kart içeriği buraya gelebilir.</p>
+              
+            </div>
+          </div>
+        </div>
 
+        <div className="col-md-3">
+          <div className="card cardEvent">
+            <img
+              src="https://uskudar.edu.tr/assets/uploads/ogrencikulup/222/uskubat-kulubu.png"
+              className="card-img-top"
+              alt="Resim"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Kart Başlık 1</h5>
+              <p className="card-text">Kart içeriği buraya gelebilir.</p>
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
