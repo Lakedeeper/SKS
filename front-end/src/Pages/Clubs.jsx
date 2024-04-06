08715a0d161a4ceb6ed1425763a6cda699afd793
softwareYusuf <112049106+softwarEngineerYusuf@users.noreply.github.com>
2024-04-06 23:39:22 +0300

    Event and Club pages have been made dynamic

--- front-end/src/Pages/Clubs.jsx
+++ front-end/src/Pages/Clubs.jsx
@@ -1,120 +1,66 @@
+import { useState, useEffect } from "react";
 import React from "react";
 import "./Clubs.css";
+
 function Clubs() {
+  const [clubs, setClub] = useState([
+    {
+      id: "1",
+      baslik: "Kart Başlık 1",
+      icerik: "Kart içeriği buraya gelebilir.",
+      resim:
+        "https://uskudar.edu.tr/assets/uploads/ogrencikulup/222/uskubat-kulubu.png",
+    },
+    {
+      id: "2",
+      baslik: "Kart Başlık 2",
+      icerik: "Kart içeriği buraya gelebilir.",
+      resim:
+        "https://uskudar.edu.tr/assets/uploads/ogrencikulup/195/insani-degerler-ve-etik-kulubu.jpg",
+    },
+    {
+      id: "2",
+      baslik: "Kart Başlık 3",
+      icerik: "Kart içeriği buraya gelebilir.",
+      resim:
+        "https://uskudar.edu.tr/assets/uploads/ogrencikulup/214/fablab-girisimcilik-kulubu.PNG",
+    },
+  ]);
+
+  useEffect(() => {
+    // axios
+    //   .get("https://api.example.com/kartlar")
+    //   .then((response) => setClub(response.data));
+  }, []);
+
   return (
     <div className="container">
+      <h1 style={{ marginTop: "75px" }}>CLUBS</h1>
       <div className="row">
-        <h1 style={{ marginTop: "75px" }}>CLUBS</h1>
-        <div className="col-md-3">
-          <div className="card">
-            <img
-              src="https://uskudar.edu.tr/assets/uploads/ogrencikulup/222/uskubat-kulubu.png"
-              alt="Resim"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Kart Başlık 1</h5>
-              <p className="card-text">Kart içeriği buraya gelebilir.</p>
-              <div>
-                <a
-                  href="#"
-                  className="btn "
-                  style={{ width: "100%", backgroundColor: "#C0E4D6" }}
-                >
-                  Daha Fazla
-                </a>
+        {clubs.map((clubs) => (
+          <div className="col-md-3" key={clubs.id}>
+            <div className="card">
+              <img
+                src={clubs.resim}
+                alt="Resim"
+                style={{ height: "300px", objectFit: "cover" }} //resimin boyutunu container a  göre ayarlamak için objectFit = cover ekledim.
+              />
+              <div className="card-body">
+                <h5 className="card-title">{clubs.baslik}</h5>
+                <p className="card-text">{clubs.icerik}</p>
+                <div>
+                  <a
+                    href="#"
+                    className="btn "
+                    style={{ width: "100%", backgroundColor: "#C0E4D6" }}
+                  >
+                    Daha Fazla
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="col-md-3">
-          <div className="card">
-            <img
-              src="https://uskudar.edu.tr/assets/uploads/ogrencikulup/222/uskubat-kulubu.png"
-              alt="Resim"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Kart Başlık 1</h5>
-              <p className="card-text">Kart içeriği buraya gelebilir.</p>
-              <div>
-                <a
-                  href="#"
-                  className="btn "
-                  style={{ width: "100%", backgroundColor: "#C0E4D6" }}
-                >
-                  Daha Fazla
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-3">
-          <div className="card">
-            <img
-              src="https://uskudar.edu.tr/assets/uploads/ogrencikulup/222/uskubat-kulubu.png"
-              alt="Resim"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Kart Başlık 1</h5>
-              <p className="card-text">Kart içeriği buraya gelebilir.</p>
-              <div>
-                <a
-                  href="#"
-                  className="btn "
-                  style={{ width: "100%", backgroundColor: "#C0E4D6" }}
-                >
-                  Daha Fazla
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-3">
-          <div className="card">
-            <img
-              src="https://uskudar.edu.tr/assets/uploads/ogrencikulup/222/uskubat-kulubu.png"
-              alt="Resim"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Kart Başlık 1</h5>
-              <p className="card-text">Kart içeriği buraya gelebilir.</p>
-              <div>
-                <a
-                  href="#"
-                  className="btn "
-                  style={{ width: "100%", backgroundColor: "#C0E4D6" }}
-                >
-                  Daha Fazla
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-3">
-          <div className="card">
-            <img
-              src="https://uskudar.edu.tr/assets/uploads/ogrencikulup/222/uskubat-kulubu.png"
-              className="card-img-top"
-              alt="Resim"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Kart Başlık 1</h5>
-              <p className="card-text">Kart içeriği buraya gelebilir.</p>
-              <div>
-                <a
-                  href="#"
-                  className="btn "
-                  style={{ width: "100%", backgroundColor: "#C0E4D6" }}
-                >
-                  Daha Fazla
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- front-end/src/Pages/Events.jsx
+++ front-end/src/Pages/Events.jsx
@@ -1,44 +1,62 @@
-import React from 'react'
-import './Events.css'
-
+import React from "react";
+import { useState, useEffect } from "react";
+import "./Events.css";
 
 function Events() {
+  const [events, setEvents] = useState([
+    {
+      id: "1",
+      baslik: "Kart Başlık 1",
+      icerik: "Kart içeriği 1",
+      resim:
+        "https://uskudar.edu.tr/assets/uploads/ogrencikulup/222/uskubat-kulubu.png",
+    },
+    {
+      id: "2",
+      baslik: "Kart Başlık 2",
+      icerik: "Kart içeriği 2",
+      resim:
+        "https://uskudar.edu.tr/assets/uploads/ogrencikulup/195/insani-degerler-ve-etik-kulubu.jpg",
+    },
+    {
+      id: "2",
+      baslik: "Kart Başlık 3",
+      icerik: "Kart içeriği 3",
+      resim:
+        "https://uskudar.edu.tr/assets/uploads/ogrencikulup/214/fablab-girisimcilik-kulubu.PNG",
+    },
+  ]);
+
+  useEffect(() => {
+    // axios
+    //   .get("https://api.example.com/kartlar")
+    //   .then((response) => setEvents(response.data));
+  }, []);
+
   return (
-
-<div className="container">
+    <div className="container">
       <div className="row">
-        <h1 className='EventsHeader' style={{ marginTop: "75px" }}>EVENTS</h1>
-        <div className="col-md-3">
-          <div className="card cardEvent">
-            <img
-              src="https://uskudar.edu.tr/assets/uploads/ogrencikulup/222/uskubat-kulubu.png"
-              alt="Resim"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Kart Başlık 1</h5>
-              <p className="card-text">Kart içeriği buraya gelebilir.</p>
-              
+        <h1 className="EventsHeader" style={{ marginTop: "75px" }}>
+          EVENTS
+        </h1>
+        {events.map((event) => (
+          <div className="col-md-3">
+            <div className="card cardEvent">
+              <img
+                src={event.resim}
+                alt="Resim"
+                style={{ height: "300px", objectFit: "cover" }} //resimin boyutunu container a  göre ayarlamak için objectFit = cover ekledim.
+              />
+              <div className="card-body">
+                <h5 className="card-title">{event.baslik}</h5>
+                <p className="card-text">{event.icerik}</p>
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="col-md-3">
-          <div className="card cardEvent">
-            <img
-              src="https://uskudar.edu.tr/assets/uploads/ogrencikulup/222/uskubat-kulubu.png"
-              className="card-img-top"
-              alt="Resim"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Kart Başlık 1</h5>
-              <p className="card-text">Kart içeriği buraya gelebilir.</p>
-             
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Events
+export default Events;
