a72acab0bd6e37febd0ada5485875afc87c8f778
Arif Halit Arslan <arifarslan@Arif-MacBook-Air.local>
2024-04-12 23:20:16 +0300

    FormAdmin has added and updated

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -7,6 +7,7 @@ import Events from "./Pages/Events";
 import Clubs from "./Pages/Clubs";
 import Form from "./Pages/Form";
 import ContactUs from "./Pages/ContactUs";
+import FormAdmin from "./Pages/FormAdmin";
 function App() {
   return (
     <BrowserRouter>
@@ -18,6 +19,7 @@ function App() {
         <Route path="/Events" element={<Events />} />
         <Route path="/Form" element={<Form/>} />
         <Route path="/ContactUs" element={<ContactUs/>} />
+        <Route path="/FormAdmin" element={<FormAdmin/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ front-end/src/Pages/FormAdmin.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./FormAdmin.css";
+
+function FormAdmin() {
+  return (
+    <>
+    <div className='FormAdminHeader'>FORMS</div>
+
+
+<div className='containestFormAdmin'>
+
+<div className='cardContainerFormAdmin'>
+    <div className='containerformAdmin'>
+        <div><img className='imgFormAdmin' src="https://uskudar.edu.tr/assets/uploads/ogrencikulup/222/uskubat-kulubu.png" alt="" /></div>
+        <div className='textFormAdmin'>Lorem ipsum dolor sit amet.</div>
+        <div className='buttonFormAdmin'><button>View</button></div>
+    </div>
+    <div className='containerformAdmin'>
+        <div><img className='imgFormAdmin' src="https://uskudar.edu.tr/assets/uploads/ogrencikulup/222/uskubat-kulubu.png" alt="" /></div>
+        <div className='textFormAdmin'>Lorem ipsum dolor sit amet.</div>
+        <div className='buttonFormAdmin'><button>View</button></div>
+    </div>
+</div>
+</div>
+    </>
+    
+  )
+}
+
+export default FormAdmin
