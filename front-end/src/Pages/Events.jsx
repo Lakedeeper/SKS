7ed6fc264ffda346b3b07dc2b79b0a360044a896
arifhalitarslan <97385377+arifhalitarslan@users.noreply.github.com>
2024-05-15 12:13:53 +0300

    .

--- front-end/src/Pages/Events.jsx
+++ front-end/src/Pages/Events.jsx
@@ -14,10 +14,12 @@ function Events() {
     const fetchData = async () => {
       try {
         const response = await axios.get("http://127.0.0.1:8000/api/forms");
-        console.log(response.data);
-        setEvents(response.data);
+        const published = response.data.filter(
+          (form) => form.state.trim() === "published"
+        );
+        setEvents(published);
         setOriginalEvents(response.data);
-        console.log(events);
+        console.log("published",published);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
